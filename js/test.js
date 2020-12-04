07fa089b4928bc74133bf5370d896da1fcf15907
a-mo <maruyoshi080920@gmail.com>
2020-12-04 10:25:21 +0900

    test

--- js/test.js
+++ js/test.js
@@ -3,9 +3,18 @@ $(function () {
     function () {
       $(".btn_star").addClass("active"), setTimeout(function () { $(".btn_star").removeClass("active") }, 500)
     }
-    ).on("click", ".btn_nico",
+  ).on("click", ".btn_nico",
     function () {
       $(".btn_nico").addClass("active"), setTimeout(function () { $(".btn_nico").removeClass("active") }, 1000)
+    }
+
+  ).on("click", ".btn_img_01",
+    function () {
+      $(".btn_img_01").addClass("active"), setTimeout(function () { $(".btn_img_01").removeClass("active") }, 1000)
+    }
+  ).on("click", ".btn_img_02",
+    function () {
+      $(".btn_img_02").addClass("active"), setTimeout(function () { $(".btn_img_02").removeClass("active") }, 1000)
     }
   );
 });
